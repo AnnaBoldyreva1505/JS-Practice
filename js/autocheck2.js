db08f49fe8ea1d2f6fd0b2a64513a49435123145
Anna Boldyreva <104367856+AnnaBoldyreva1505@users.noreply.github.com>
2023-01-15 00:44:48 +0600

    Update autocheck2.js

--- js/autocheck2.js
+++ js/autocheck2.js
@@ -166,17 +166,350 @@ console.log(fruits.at(-1));
 //  console.log(calculateEngravingPrice("JavaScript is in my blood", 10));
 // ================= Задача 12 join() ==============
 // Дополни код функции makeStringFromArray(array, delimiter) так, чтобы она возвращала в переменной string результат соединения элементов массива array c разделителем delimiter - строку.
-function makeStringFromArray(array, delimiter) {
-    let string;
-    // Change code below this line
+// function makeStringFromArray(array, delimiter) {
+//     let string;
+//     // Change code below this line
   
-  return string = array.join(delimiter)
+//   return string = array.join(delimiter)
   
-    // Change code above this line
-    return string;
+//     // Change code above this line
+//     return string;
+//   }
+
+// // Метод массивов join(delimiter) позволяет соединить элементы массива в строку. В строке элементы будут разделены символом или группой символов указанных в delimiter. То есть это операция обратная методу строк split(delimiter).
+
+
+// function initSnaker(str) {
+  
+// }
+
+
+// console.log(initSnaker(CamelCase));
+
+
+// ================= Задача 13 ==============
+
+// Термин slug - это человеко-понятный уникальный идентификатор, который используется в веб-разработке для создания читабельных URL-адресов.
+
+// Например, вместо того чтобы пользователь увидел в адресной строке mysite.com/posts/1q8fh74tx, можно сделать slug из названия статьи. В результате адрес получится более приятным для восприятия: mysite.com/posts/arrays-for-begginers.
+
+// Внимание
+// Slug это всегда строка в нижнем регистре, слова которой разделены тире.
+
+// Напиши функцию slugify(title) которая принимает заголовок статьи, параметр title, и возвращает slug, созданный из этой строки.
+
+// Значением параметра title будут строки, слова которых разделены только пробелами
+// Все символы slug должны быть в нижнем регистре
+// Все слова slug должна быть разделены тире
+
+// function slugify(title) {
+//   // Change code below this line
+
+// return title.split(" ").join("-").toLowerCase()
+
+//   // Change code above this line
+// }
+
+
+
+// ================= Задача 14 ==============
+
+
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// // Change code below this line
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, 4);
+// const lastThreeEls = fruits.slice(-3);
+
+// firstTwoEls - массив из первых двух элементов
+// nonExtremeEls - массив из всех элементов кроме первого и последнего
+// lastThreeEls - массив из трёх последних элементов
+
+
+// ================= Задача 15 concat - соеденить 2 массива==============
+
+// Дополни код так, чтобы в переменной allClients получился массив всех элементов массивов oldClients и newClients.
+// const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// const newClients = ['Peach', 'Houston'];
+
+// const allClients = oldClients.concat(newClients); // Change this line
+
+// ================= Задача 16 ==============
+
+// Напиши функцию makeArray(firstArray, secondArray, maxLength) для создания нового массива со всеми элементами двух исходных firstArray и secondArray. Параметр maxLength содержит максимально допустимую длину нового массива.
+
+// Если количество элементов нового массива больше maxLength, функция должна вернуть копию массива длиной maxLength элементов. В противном случае функция должна вернуть новый массив целиком.
+
+// if (firstArray.concat(secondArray).length > maxLength) {
+//   return firstArray.concat(secondArray).slice(0, maxLength);
+// } 
+
+//   return firstArray.concat(secondArray);
+
+// function makeArray(firstArray, secondArray, maxLength) {
+//   // Change code below this line
+
+// // if (firstArray.concat(secondArray).length > maxLength) {
+// //   return firstArray.concat(secondArray).slice(0, maxLength);
+// // } 
+
+// //   return firstArray.concat(secondArray);
+
+// return  firstArray.concat(secondArray).length > maxLength ? firstArray.concat(secondArray).slice(0, maxLength) : firstArray.concat(secondArray)
+
+//   // Change code above this line
+// }
+
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+
+
+// ================= Задача 17 Цикл For ==============
+
+// Дополни цикл for так, чтобы он логировал все целые числа в диапазоне от start до end включительно.
+// const start = 3;
+// const end = 7;
+
+// for (let i = start; i <= end; i +=1 ) { // Change this line
+//   console.log(i);
+// }
+
+// ================= Задача 18 Цикл For==============
+
+// function calculateTotal(number) {
+//   // Change code below this line
+//  let sum = 0
+// for (let index = 0; index <= number; index += 1) {
+//  sum += index;  
+
+// console.log(index);
+// }
+// //return  sum
+// console.log(sum);
+//    // Change code above this line
+//  }
+//  calculateTotal(5)
+// ================= Задача 19 ==============
+
+// Дополни код цикла for так, чтобы он последовательно логировал все элементы массива fruits.
+
+// const fruitis = ['apple', 'plum', 'pear', 'orange'];
+
+// for (let i = 0; i < fruits.length; i+=1) { // Change this line
+//   const fruit = fruits[i]; // Change this line
+//   console.log(fruit);
+//   console.log(i);
+// }
+
+// ================= Задача 20 ==============
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+// for (let index = 0; index < order.length; index += 1) {
+//   total += order[index]
+// }
+//   // Change code above this line
+//   return total;
+// }
+// Напиши функцию calculateTotalPrice(order), которая принимает один параметр order - массив чисел, и рассчитывает общую сумму его элементов. Общая сумма элементов должна сохраняться в переменной total, которая возвращается, как результат работы функции.
+
+// ================= Задача 21 ==============
+
+// function findLongestWord(string) {
+// //   // Change code below this line
+
+// // const getWords = string.split(" ")
+// // console.log(getWords);
+
+// // getWords.reduce()
+
+// // let longeestWord
+
+
+// // for (let index = 0; index < getWords.length; index++) {
+// //   const element = getWords[index].length;
+// //   // console.log(getWords[index]);
+// //   console.log(element);
+  
+
+// // }
+
+
+
+
+// //   // Change code above this line
+// // }
+
+// // findLongestWord("The quick brown fox jumped over the lazy dog")
+// // ================= Задача 22 ==============
+
+// // function createArrayOfNumbers(min, max) {
+// //   const numbers = [];
+// //   // Change code below this line
+// //  for(let i = min;  i <= max; i++) {
+// //     numbers.push(i)
+// //     console.log(numbers.push(i));
+// //   }
+// //   // Change code above this line
+// //   console.log(numbers);
+  
+  
+// // }
+
+// // createArrayOfNumbers(5, 10)
+
+// // ================= Задача 23 ==============
+
+// // function filterArray(numbers, value) {
+// //   // Change code below this line
+// // const newArr = []
+
+// //  for (let index = 0; index < numbers.length; index++) {
+
+// // if(numbers[index] > value)
+// //    newArr.push(numbers[index])
+// //  }
+
+
+// //  // Change code above this line
+// // }
+
+// // console.log(filterArray([1, 2, 3, 4, 5], 3));
+
+
+// // ================= Задача 24 Includes()==============
+// function checkFruit(fruit) {
+//   const fruits = ["apple", "plum", "pear", "orange"];
+
+// return fruits.includes(fruit)
+  
+//   return; // Change this line
+// }
+
+
+// function checkFruit(fruit) {
+//   const fruits = ["apple", "plum", "pear", "orange"];
+
+// return fruits.includes(fruit)
+  
+//   return; // Change this line
+// }
+
+
+// // ================= Задача 25 Общие элементы ==============
+
+// function getCommonElements(array1, array2) {
+//   let result = [];
+//     for (let i = 0; i < array1.length; i++) {
+//       if (array2.includes(array1[i])) {
+//         result.push(array1[i]);
+//       }
+//     }
+//     return result;
+//   }
+
+//   getCommonElements([1, 2, 3], [2, 4])
+// ================= Задача 26 for...of ==============
+
+// const planets = ["Earth", "Mars", "Venus"];
+
+// for (const planet of planets) {
+//   console.log(planet);
+// }
+
+// const num = [5, 8, 10];
+// let sum = 0
+// for (let n of num) {
+
+//  sum += n;
+// }
+// console.log(sum);
+
+// ================= Задача 27 ==============
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+
+  
+
+//   for (a of order) {
+//       total += a}
+
+//   // Change code above this line
+//   return total;
+// }
+
+// ================= Задача 28 ==============
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   const filteredNumbers = [];
+
+//   for (let i of numbers) {
+//     const number = i;
+
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//   }
+//   }
+//   return filteredNumbers;
+//   // Change code above this line
+// }
+
+// ================= Задача 29 ==============
+// const a = 3 % 3;
+// const b = 4 % 3;
+// const c = 11 % 4;
+// const d = 12 % 7;
+// const e = 8 % 6;
+
+
+// ================= Задача 30 ==============
+function getEvenNumbers(start, end) {
+  let newArr = []
+  
+  for (let index = start; index <= end; index += 1) {
+    if(index % 2 === 0){
+     newArr.push(index)
+   }
+  }
+  
+  
+    
+  return newArr
+  
+  
   }
 
+// ================= Задача 31 break==============
+// Дополни код так, чтобы в переменную number записывалось первое число от start до end, которое делится на 5 без остатка.
+//   const start = 6;
+// const end = 27;
+// let number;
+
+// for (let i = start; i < end; i += 1) {
+//   if (i % 5 === 0) {
+//     number = i;
+//       break;
+//   }
+// }
+    
 
 
 
-Метод массивов join(delimiter) позволяет соединить элементы массива в строку. В строке элементы будут разделены символом или группой символов указанных в delimiter. То есть это операция обратная методу строк split(delimiter).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
