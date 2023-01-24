b94867da2fd3d947f6f28e87603647b90ffad08b
Anna Boldyreva <104367856+AnnaBoldyreva1505@users.noreply.github.com>
2023-01-25 01:28:38 +0600

    done 3

--- js/autocheck3.js
+++ js/autocheck3.js
@@ -619,3 +619,336 @@
 //  const { 
 //    today: { low: lowToday, high: highToday, icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"},
 //    tomorrow: {low: lowTomorrow, high: highTomorrow, icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"},} = forecast;
+
+
+
+// =============== Задача 26 Объекты ===========
+
+// Функция calculateMeanTemperature(forecast) принимает один параметр forecast - объект температур на два дня следующего формата.
+
+// {
+//   today: { low: 10, high: 20 },
+//   tomorrow: { low: 20, high: 30 }
+// }
+// Замени объявления переменных todayLow, todayHigh, tomorrowLow и tomorrowHigh одной операцией деструктуризации свойств объекта forecast.
+
+// Change code below this line
+// function calculateMeanTemperature(forecast) {
+
+//    const {today: {low:todayLow, high:todayHigh}, tomorrow: {low:tomorrowLow, high:tomorrowHigh}} = forecast
+   
+ 
+ 
+//    // Change code above this line
+//    return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+//  }
+
+
+// =============== Задача 27 Объекты ===========
+
+// Синтаксис ... (spread) позволяет распылить коллекцию элементов (массив, строку или объект) в место, где ожидается набор отдельных значений. Конечно есть некоторые ограничения, например нельзя распылить массив в объект и наоборот.
+
+// Можно привести аналогию с ящиком яблок. Поставив ящик на пол не вынимая из него яблоки, получим аналог массива значений. Если высыпать яблоки из ящика на пол, произойдёт распыление - набор отдельных значений.
+
+// Отличие всего одно - в JavaScript распыление не изменяет оригинальную коллекцию, то есть делается копия каждого элемента. После распыления останется и ящик полный яблок, и копия каждого яблока на полу.
+
+// В переменной scores хранится массив результатов тестирования. Используя распыление и методы Math.max() и Math.min() дополни код так, чтобы в переменной bestScore был самый высокий балл, а в worstScore самый низкий.
+
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+// // Change code below this line
+// const bestScore = Math.max(...scores);
+// const worstScore = Math.min(...scores);
+
+// console.log(bestScore);
+// console.log(worstScore);
+// =============== Задача 28 Объекты spread===========
+
+// Операция spread позволяет создать копию массива или «склеить» произвольное количество массивов в один новый. Раньше для этого использовали методы slice() и concat(), но операция распыления позволяет сделать тоже самое в более краткой форме.
+
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Change code below this line
+// const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+// const bestScore = Math.max(...allScores);
+// const worstScore = Math.min(...allScores) ;
+
+// console.log(allScores);
+// console.log(bestScore);
+// console.log(worstScore);
+// =============== Задача 29 Объекты ===========
+// В конструкторе можно создавать новые тесты, для которых есть настройки по умолчанию которые хранятся в переменной defaultSettings. Во время создания теста, все или часть настроек можно переопределить, они хранятся в переменной overrideSettings.
+
+// Для того чтобы получить финальные настройки теста, необходимо взять настройки по умолчанию и поверх них применить переопределённые настройки. Дополни код так, чтобы в переменной finalSettings получился объект финальных настроек теста.
+
+// const defaultSettings = {
+//    theme: "light",
+//    public: true,
+//    withPassword: false,
+//    minNumberOfQuestions: 10,
+//    timePerQuestion: 60,
+//  };
+//  const overrideSettings = {
+//    public: false,
+//    withPassword: true,
+//    timePerQuestion: 30,
+//  };
+//  // Change code below this line
+//  const finalSettings = {...defaultSettings, ...overrideSettings};
+// =============== Задача 30 Объекты ===========
+// Напиши функцию makeTask(data) которая принимает один параметр data - объект со следующими свойствами.
+
+// text - текст задачи.
+// category - категория задачи.
+// priority - приоритет задачи.
+// Функция должна составить и вернуть новый объект задачи, не изменяя напрямую параметр data. В новом объекте должно быть свойство completed, значение которого хранится в одноимённой локальной переменной.
+
+// В параметре data гарантированно будет только свойство text, а остальные два, category и priority, могут отсутствовать. Тогда, в новом объекте задачи, в свойствах category и priority должны быть значения по умолчанию, хранящиеся в одноимённых локальных переменных.
+// function makeTask(data) {
+//    const completed = false;
+//    const category = "General";
+//    const priority = "Normal";
+//    // Change code below this line
+//   return {completed, category, priority, ...data};
+ 
+
+  
+//    // Change code above this line
+//  }
+
+//  makeTask({ category: "Homemade", priority: "Low", text: "Take out the trash" })
+
+//  console.log( makeTask({}));
+
+// =============== Задача 31 Объекты ===========
+// Используя операцию rest дополни код функции add() так, чтобы она принимала любое количество аргументов, считала и возвращала их сумму.
+// // Change code below this line
+// function add(...args) {
+//    // Change code above this line
+ 
+//    let sum = 0;
+ 
+//    for (const arg of args) {
+//     sum += arg
+ 
+//    }
+//    return sum
+//  }
+// =============== Задача 32 Объекты ===========
+// Операция ... (rest) также позволяет собрать в массив только ту часть аргументов, которая необходима, объявив параметры до «сбора».
+
+// function multiply(firstNumber, secondNumber, ...otherArgs) {
+//   console.log(firstNumber); // Значение первого аргумента
+//   console.log(secondNumber); // Значение второго аргумента
+//   console.log(otherArgs); // Массив остальных аргументов
+// }
+
+// multiply(1, 2);
+// multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+// Все аргументы, для которых будут объявлены параметры, передадут им свои значения, остальные аргументы будут помещены в массив. Операция rest собирает все оставшиеся аргументы и поэтому должна идти последней в подписи функции, иначе будет ошибка.
+
+// Функция addOverNum() считает сумму всех аргументов. Измени параметры и тело функции addOverNum() так, чтобы она считала сумму только тех аргументов, которые больше чем заданное число. Это число должно быть первым параметром функции.
+// // Change code below this line
+// function addOverNum(firstNumber, ...args) {
+//    let total = 0;
+ 
+//    for (const arg of args) {
+ 
+//      if(firstNumber < arg)
+//      total += arg;
+//    }
+ 
+//    return total;
+//    // Change code above this line
+//  }
+ 
+// =============== Задача 33 Объекты ===========
+// Change code below this line
+// function findMatches(first, ...args) {
+//    const matches = []; // Don't change this line
+//    for (const arg of args)
+//      if (first.includes(arg)) {
+//        matches.push(arg)
+//      }
+//  console.log(matches)
+//    // Change code above this line
+//    return matches;
+//  }
+//  Функция findMatches() принимает произвольное количество аргументов. Первым аргументом всегда будет массив чисел, а остальные аргументы будут просто числами.
+
+//  Дополни код функции так, чтобы она возвращала новый массив matches, в котором будут только те аргументы, начиная со второго, которые есть в массиве первого аргумента.
+ 
+//  Например, findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7) должна вернуть массив [1, 2], потому что только они есть в массиве первого аргумента. 
+
+// =============== Задача 34 Объекты ===========
+// Добавь объекту bookShelf ещё два метода, которые пока что будут возвращать просто строки по аналогии с getBooks() и addBook(bookName).
+
+// Метод removeBook(bookName) будет удалять книгу по имени. Возвращает строку "Deleting book <имя книги>", где <имя книги> это значение параметра bookName.
+
+// Метод updateBook(oldName, newName) будет обновлять название книги на новое. Возвращает строку "Updating book <старое имя> to <новое имя>", где <старое имя> и <новое имя>это значения параметров oldName и newName соотвественно.
+// const bookShelf = {
+//    // Change code below this line
+//    books: ["The last kingdom", "The guardian of dreams"],
+//    getBooks() {
+//      return "Returning all books";
+//    },
+//    addBook(bookName) {
+//      return `Adding book ${bookName}`;
+//    },
+ 
+//    removeBook(bookName) {
+//    return  `Deleting book ${bookName}`
+//    },
+ 
+//    updateBook(oldName, newName) {
+//    return  `Updating book ${oldName} to ${newName}`
+//    }
+//    // Change code above this line
+//  };
+// =============== Задача 35 Объекты ===========
+const bookShelf = {
+   books: ["The last kingdom", "Haze", "The guardian of dreams"],
+   updateBook(oldName, newName) {
+     // Change code below this line
+ 
+  return      this.books.splice(this.books.indexOf(oldName), 1, newName)
+ 
+     // Change code above this linereturn re
+   },
+ };
+ 
+
+// =============== Задача 36 Объекты ===========
+
+// К нам обратилась владелица лавки зелий «У старой жабы» и заказала программу для ведения инвентаря - добавления, удаления, поиска и обновления зелий. Добавь объекту atTheOldToad свойство potions, значением которого сделай пустой массив.
+
+
+// const atTheOldToad = {
+//    // Change code below this line
+ 
+//  potions: [] 
+ 
+//    // Change code above this line
+//  };
+
+
+//  добавь объекту atTheOldToad метод getPotions(), который просто возвращает значение свойства potions.
+
+// const atTheOldToad = {
+//    potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//    // Change code below this line
+//  getPotions() {
+//    return this.potions
+//  }
+//    // Change code above this line
+//  };
+
+// Дополни метод addPotion(potionName) так, чтобы он добавлял зелье potionName в конец массива зелий в свойстве potions.
+
+// const atTheOldToad = {
+//    potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//    addPotion(potionName) {
+//      // Change code below this line
+ 
+//  this.potions.push(potionName)
+ 
+//      // Change code above this line
+//    },
+//  };
+
+// Дополни метод removePotion(potionName) так, чтобы он удалял зелье potionName из массива зелий в свойстве potions.
+// const atTheOldToad = {
+//    potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//    removePotion(potionName) {
+//      // Change code below this line
+ 
+//  let deletedPotions = this.potions.indexOf(potionName);
+
+//      this.potions.splice(deletedPotions, 1)
+ 
+//      // Change code above this line
+//    },
+//  };
+
+
+
+// const atTheOldToad = {
+//    potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//    updatePotionName(oldName, newName) {
+//      // Change code below this line
+ 
+//  const oldNameFind = this.potions.indexOf(oldName)
+//      return this.potions.splice(oldNameFind, 1, newName)
+//      // Change code above this line
+//    },
+//  };
+
+//  Дополни метод updatePotionName(oldName, newName) так, чтобы он обновлял название зелья с oldName на newName, в массиве зелий в свойстве potions.
+
+
+
+
+// const atTheOldToad = {
+//    potions: [
+//      { name: "Speed potion", price: 460 },
+//      { name: "Dragon breath", price: 780 },
+//      { name: "Stone skin", price: 520 },
+//    ],
+//    // Change code below this line
+//    getPotions() {
+//      return this.potions;
+//    },
+//   addPotion(newPotion) {
+//      for (let potion of this.potions) {
+ 
+//      if (potion.name === newPotion.name) {
+//        return `Error! Potion ${newPotion.name} is already in your inventory!`;
+ 
+//      }
+//      }
+//      this.potions.push(newPotion)
+//    },
+//    removePotion(potionName) {
+    
+//  for (let potion of this.potions) {
+//    if (potion.name === potionName) {
+//      const index = this.potions.indexOf(potion);
+//       this.potions.splice(index, 1);
+//    }
+//  }
+ 
+//    `Potion ${potionName} is not in inventory!`;
+    
+//    },
+//   updatePotionName(oldName, newName) {
+ 
+//     for (let potion of this.potions) {
+   
+//       if (oldName === potion.name) {
+//         potion.name = newName
+//       }
+  
+//  }
+ 
+//      return `Potion ${oldName} is not in inventory!`;
+//    },
+//    // Change code above this line
+//  };
+
+//  Заказчица хочет чтобы каждое зелье было представлено не только именем, но и ценой, а в будущем может быть и другими характеристиками. Поэтому теперь в свойстве potions будет храниться массив объектов со следующими свойствами.
+
+// {
+//   name: "Dragon breath",
+//   price: 700
+// }
+// Выполни рефакторинг методов объекта atTheOldToad так, чтобы они работали не с массивом строк, а с массивом объектов.
+
+// getPotions() - метод для получения всех зелий. Возвращает значение свойства potions.
+// addPotion(newPotion) - добавляет зелье newPotion (уже объект) в массив в свойстве potions, но только если такого зелья еще нет в инвентаре. В противном случае возвращается строка.
+// removePotion(potionName) - удаляет объект зелья с именем potionName из массива в свойстве potions.
+// updatePotionName(oldName, newName) - обновляет свойство name объекта-зелья с названием oldName на newName в массиве potions.
+
+
+
+ 
